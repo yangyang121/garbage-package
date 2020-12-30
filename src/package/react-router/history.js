c395b1856c8be19e00cceeb26f964640546d9a3c
yangyang121 <844171995@qq.com>
2020-12-30 21:16:14 +0800

    update

--- src/package/react-router/history.js
+++ src/package/react-router/history.js
@@ -15,14 +15,22 @@ function createEvents() {
 
 export function createBrowserHistory() {
   const listeners = createEvents()
-  let location = {
-    pathname: "/",
+  function getState() {
+    return window.history.state || {}
   }
+  function getLoacation() {
+    const { state } = getState()
+    const { pathname, search } = window.location
+    return {
+      state,
+      pathname,
+      search,
+    }
+  }
+  let location = getLoacation()
 
   const handlePop = function () {
-    const currentLocation = {
-      pathname: window.location.pathname,
-    }
+    const currentLocation = getLoacation()
     listeners.call(currentLocation)
   }
 
@@ -32,15 +40,39 @@ export function createBrowserHistory() {
     listen(listener) {
       return listeners.push(listener)
     },
-    push(url) {
-      const history = window.history
+    push(path) {
+      if (typeof path === "string") {
+        // Two-arg form: push(path, state)
+        location.pathname = path
+        location.state = ""
+        location.search = undefined
+      } else {
+        // One-arg form: push(location)
+        location = { ...path }
+
+        if (location.pathname === undefined) location.pathname = ""
+
+        if (location.search) {
+          if (location.search.charAt(0) !== "?")
+            location.search = "?" + location.search
+        } else {
+          location.search = ""
+        }
+      }
+
+      if (!location.pathname) {
+        location.pathname = "/"
+      }
+
       // 这里pushState并不会触发popstate
       // 但是我们仍然要这样做，是为了保持state栈的一致性
-      history.pushState(null, "", url)
+      window.history.pushState(
+        location.state,
+        "",
+        location.pathname + (location.search || "")
+      )
       // 由于push并不触发popstate，我们需要手动调用回调函数
-      location = { pathname: url }
       listeners.call(location)
-      console.log(location)
     },
     get location() {
       return location
