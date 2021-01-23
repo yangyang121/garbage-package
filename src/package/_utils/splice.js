2d44017123c1045a0053dbbf1ff0a6a4f5dbec38
yangyang121 <844171995@qq.com>
2021-01-23 18:01:41 +0800

    update

--- src/package/_utils/splice.js
+++ src/package/_utils/splice.js
@@ -1,90 +1,95 @@
-export default function splice(array, start, deleteCount, ...args) {
-  // 入参元素个数
-  let argumentsLen = args.length
-  // 数组长度
-  let len = array.length
-  // 添加元素个数
-  let addCount = argumentsLen
-  // 计算有效的 start
-  let startIndex = computeSpliceStartIndex(start, array)
-  // 计算有效的 deleteCount
-  let delCount = computeSpliceDeleteCount(startIndex, deleteCount, len)
-  // 记录删除的数组元素
-  let deletedElements = new Array(delCount)
-
-  // 将待删除元素记录到 deletedArray
-  recordDeleteElements(startIndex, delCount, array, deletedElements)
-
-  // 密封对象
-  if (delCount !== addCount && Object.isSealed(array)) {
-    throw new TypeError("the array is sealed")
+const sliceDeleteElements = (array, startIndex, deleteCount, deleteArr) => {
+  for (let i = 0; i < deleteCount; i++) {
+    let index = startIndex + i
+    if (index in array) {
+      let current = array[index]
+      deleteArr[i] = current
+    }
   }
-  // 冻结对象
-  if (delCount > 0 && addCount > 0 && Object.isFrozen(array)) {
-    throw new TypeError("the array is frozen")
-  }
-
-  // 移动数组元素
-  moveElements(startIndex, delCount, array, addCount)
-
-  let i = startIndex
-  let argumentsIndex = 0
-
-  // 插入新元素
-  while (argumentsIndex < argumentsLen) {
-    array[i++] = arguments[argumentsIndex++]
-  }
-
-  array.length = len - delCount + addCount
-
-  // 返回删除元素数组
-  return deletedElements
 }
 
-// 计算真实的 start
-function computeSpliceStartIndex(start, len) {
-  // 处理负值，如果负数的绝对值大于数组的长度，则表示开始位置为第0位
-  if (start < 0) {
-    start += len
-    return start < 0 ? 0 : start
+const movePostElements = (array, startIndex, len, deleteCount, addElements) => {
+  // 如果添加的元素和删除的元素个数相等，相当于元素的替换，数组长度不变，被删除元素后面的元素不需要挪动
+  if (deleteCount > addElements.length) {
+    // 删除的元素比新增的元素多，那么后面的元素整体向前挪动
+    // 一共需要挪动 len - startIndex - deleteCount 个元素
+    for (let i = startIndex + deleteCount; i < len; i++) {
+      let fromIndex = i
+      // 将要挪动到的目标位置
+      let toIndex = i - (deleteCount - addElements.length)
+      if (fromIndex in array) {
+        array[toIndex] = array[fromIndex]
+      } else {
+        delete array[toIndex]
+      }
+    }
+    // 注意注意！这里我们把后面的元素向前挪，相当于数组长度减小了，需要删除冗余元素
+    // 目前长度为 len + addElements - deleteCount
+    for (let i = len - 1; i >= len + addElements.length - deleteCount; i--) {
+      delete array[i]
+    }
+  } else if (deleteCount < addElements.length) {
+    // 删除的元素比新增的元素少，那么后面的元素整体向后挪动
+    for (let i = len - 1; i >= startIndex + deleteCount; i--) {
+      let fromIndex = i
+      // 将要挪动到的目标位置
+      let toIndex = i + (addElements.length - deleteCount)
+      if (fromIndex in array) {
+        array[toIndex] = array[fromIndex]
+      } else {
+        delete array[toIndex]
+      }
+    }
   }
-  // 处理超出边界问题
-  return start > len - 1 ? len - 1 : start
 }
 
-// 计算真实的 deleteCount
-function computeSpliceDeleteCount(startIndex, deleteCount, len) {
-  // 超出边界问题
-  if (deleteCount > len - startIndex) deleteCount = len - startIndex
-  // 负值问题
-  if (deleteCount < 0) deleteCount = 0
+const computeStartIndex = (startIndex, len) => {
+  // 处理索引负数的情况
+  if (startIndex < 0) {
+    return startIndex + len > 0 ? startIndex + len : 0
+  }
+  return startIndex >= len ? len : startIndex
+}
+
+const computeDeleteCount = (startIndex, len, deleteCount, argumentsLen) => {
+  // 删除数目没有传，默认删除startIndex及后面所有的
+  if (argumentsLen === 2) return len - startIndex
+  // 删除数目过小
+  if (deleteCount < 0) return 0
+  // 删除数目过大
+  if (deleteCount > len - startIndex) return len - startIndex
   return deleteCount
 }
 
-// 记录删除元素，用于 Array.prototype.splice() 返回
-function recordDeleteElements(startIndex, delCount, array, deletedElementd) {
-  for (let i = 0; i < delCount; i++) {
-    deletedElementd[i] = array[startIndex + i]
-  }
-}
+export default function splice(array, startIndex, deleteCount, ...addElements) {
+  let argumentsLen = arguments.length
+  let len = array.length
 
-// 移动数组元素，便于插入新元素
-function moveElements(startIndex, delCount, array, addCount) {
-  let over = addCount - delCount
-  if (over) {
-    // 向后移
-    for (let i = array.length - 1; i >= startIndex + delCount; i--) {
-      array[i + over] = array[i]
-    }
-  } else if (over < 0) {
-    // 向前移
-    for (let i = startIndex + delCount; i <= array.length - 1; i++) {
-      if (i + Math.abs(over) > array.length - 1) {
-        // 删除冗于元素
-        delete array[i]
-        continue
-      }
-      array[i] = array[i + Math.abs(over)]
-    }
+  startIndex = computeStartIndex(startIndex, len)
+  deleteCount = computeDeleteCount(startIndex, len, deleteCount, argumentsLen)
+  let deleteArr = new Array(deleteCount)
+
+  // 判断 sealed 对象和 frozen 对象, 即 密封对象 和 冻结对象
+  if (Object.isSealed(array) && deleteCount !== addElements.length) {
+    throw new TypeError("the object is a sealed object!")
+  } else if (
+    Object.isFrozen(array) &&
+    (deleteCount > 0 || addElements.length > 0)
+  ) {
+    throw new TypeError("the object is a frozen object!")
   }
+
+  // 拷贝删除的元素
+  sliceDeleteElements(array, startIndex, deleteCount, deleteArr)
+  // 移动删除元素后面的元素
+  movePostElements(array, startIndex, len, deleteCount, addElements)
+
+  // 插入新元素
+  for (let i = 0; i < addElements.length; i++) {
+    array[startIndex + i] = addElements[i]
+  }
+
+  array.length = len - deleteCount + addElements.length
+
+  return deleteArr
 }
